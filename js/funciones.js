6fe6a3d690356cdc606e07cbd3ee073351479f12
eachitanc <eachitanc@gmail.com>
2020-05-21 12:53:52 -0500

    Proyecto materialize

---
+++ js/funciones.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.carousel').carousel();
+
+    $('.fixed-action-btn').floatingActionButton();
+
+    $('.modal').modal();
+
+    $('.slider').slider();
+
+    $('.dropdown-trigger').dropdown();
+});
+function mensaje1(){
+    document.getElementById("texto").innerHTML= "Somos la organización sin ánimo de lucro que se dedica a la recuperación de envases en toda Colombia, Creemos en la eficiencia medioambiental y económica, y trabajamos para conseguir que los resultados de reciclaje de envases en Colombia crezcan año tras año, hasta llegar a ser uno de los países Latinos de referencia en esta materia.";
+}
+function mensaje2(){
+    document.getElementById("texto").innerHTML= "Envia tu PQSR al correo: no_existe@nada.com";
+}
